fd1bdacb076c99430f2b25cab6668a87b96416a3
Erik Bremstedt <erik.bremstedt@gmail.com>
2024-12-29 15:13:04 +0100

    fix init file

---
+++ stormsvala/__init__.py
@@ -0,0 +1,12 @@
+from stormsvala.column import Column
+from stormsvala.driver import PostgresDriver
+from stormsvala.path import Path
+from stormsvala.table import Table
+
+
+__all__ = [
+    "Column",
+    "PostgresDriver",
+    "Path",
+    "Table",
+]
